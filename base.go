74cf339846587482d8010b486e9601ff6fb4bb8f
Lunny Xiao <xiaolunwen@gmail.com>
2014-04-15 12:09:45 +0800

    add incr tests

--- base.go
+++ base.go
@@ -484,6 +484,54 @@ func update(engine *xorm.Engine, t *testing.T) {
 			return
 		}
 	}
+
+	{
+		type UpdateIncr struct {
+			Id  int64
+			Cnt int
+		}
+
+		col1 := &UpdateIncr{}
+		err = engine.Sync(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		_, err = engine.Insert(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		cnt, err := engine.Id(1).Inc("cnt", 1).Update(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+		if cnt != 1 {
+			err = errors.New("update incr failed")
+			t.Error(err)
+			panic(err)
+		}
+
+		newCol := new(UpdateIncr)
+		has, err := engine.Id(1).Get(newCol)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+		if !has {
+			err = errors.New("has incr failed")
+			t.Error(err)
+			panic(err)
+		}
+		if 1 != newCol.Cnt {
+			err = errors.New("incr failed")
+			t.Error(err)
+			panic(err)
+		}
+	}
 }
 
 func updateSameMapper(engine *xorm.Engine, t *testing.T) {
@@ -532,6 +580,198 @@ func updateSameMapper(engine *xorm.Engine, t *testing.T) {
 		t.Error(err)
 		panic(err)
 		return
+	}
+
+	err = engine.Sync(&Article{})
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	defer func() {
+		err = engine.DropTables(&Article{})
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+	}()
+
+	a := &Article{0, "1", "2", "3", "4", "5", 2}
+	cnt, err = engine.Insert(a)
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	if cnt != 1 {
+		err = errors.New(fmt.Sprintf("insert not returned 1 but %d", cnt))
+		t.Error(err)
+		panic(err)
+	}
+
+	if a.Id == 0 {
+		err = errors.New("insert returned id is 0")
+		t.Error(err)
+		panic(err)
+	}
+
+	cnt, err = engine.Id(a.Id).Update(&Article{Name: "6"})
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	if cnt != 1 {
+		err = errors.New(fmt.Sprintf("insert not returned 1 but %d", cnt))
+		t.Error(err)
+		panic(err)
+		return
+	}
+
+	type UpdateAllCols struct {
+		Id     int64
+		Bool   bool
+		String string
+	}
+
+	col1 := &UpdateAllCols{}
+	err = engine.Sync(col1)
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	_, err = engine.Insert(col1)
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	col2 := &UpdateAllCols{col1.Id, true, ""}
+	_, err = engine.Id(col2.Id).AllCols().Update(col2)
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	col3 := &UpdateAllCols{}
+	has, err := engine.Id(col2.Id).Get(col3)
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	if !has {
+		err = errors.New(fmt.Sprintf("cannot get id %d", col2.Id))
+		t.Error(err)
+		panic(err)
+		return
+	}
+
+	if *col2 != *col3 {
+		err = errors.New(fmt.Sprintf("col2 should eq col3"))
+		t.Error(err)
+		panic(err)
+		return
+	}
+
+	{
+		type UpdateMustCols struct {
+			Id     int64
+			Bool   bool
+			String string
+		}
+
+		col1 := &UpdateMustCols{}
+		err = engine.Sync(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		_, err = engine.Insert(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		col2 := &UpdateMustCols{col1.Id, true, ""}
+		boolStr := engine.ColumnMapper.Obj2Table("Bool")
+		stringStr := engine.ColumnMapper.Obj2Table("String")
+		_, err = engine.Id(col2.Id).MustCols(boolStr, stringStr).Update(col2)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		col3 := &UpdateMustCols{}
+		has, err := engine.Id(col2.Id).Get(col3)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		if !has {
+			err = errors.New(fmt.Sprintf("cannot get id %d", col2.Id))
+			t.Error(err)
+			panic(err)
+			return
+		}
+
+		if *col2 != *col3 {
+			err = errors.New(fmt.Sprintf("col2 should eq col3"))
+			t.Error(err)
+			panic(err)
+			return
+		}
+	}
+
+	{
+		type UpdateIncr struct {
+			Id  int64
+			Cnt int
+		}
+
+		col1 := &UpdateIncr{}
+		err = engine.Sync(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		_, err = engine.Insert(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+
+		cnt, err := engine.Id(1).Inc("`Cnt`", 1).Update(col1)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+		if cnt != 1 {
+			err = errors.New("update incr failed")
+			t.Error(err)
+			panic(err)
+		}
+
+		newCol := new(UpdateIncr)
+		has, err := engine.Id(1).Get(newCol)
+		if err != nil {
+			t.Error(err)
+			panic(err)
+		}
+		if !has {
+			err = errors.New("has incr failed")
+			t.Error(err)
+			panic(err)
+		}
+		if 1 != newCol.Cnt {
+			err = errors.New("incr failed")
+			t.Error(err)
+			panic(err)
+		}
 	}
 }
 
