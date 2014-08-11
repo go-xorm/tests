3a0381b60964f3c3c78b4f4cb6149776fa709647
Nash Tsai <nash.tsai@gmail.com>
2014-08-11 20:03:58 +0800

    remove echo

--- dynamic_path_test.sh
+++ dynamic_path_test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 this_file=$(basename $0)
 base_dir_len=`expr ${#this_file} - 3`
-echo $base_dir_len
 test_dir=${this_file: 0: `expr $base_dir_len`}
-echo $test_dir
-echo "cd to $test_dir"
 cd $test_dir
 ./$this_file
 cd -
